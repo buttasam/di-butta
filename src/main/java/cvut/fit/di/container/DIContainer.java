6f07aa2119c16ceb8558c187d6071dabc10bd689
Samuel Butta <samuel@butta.cz>
2017-03-09 17:42:52 +0100

    Zakladni rozhrani DIContainer

---
+++ src/main/java/cvut/fit/di/container/DIContainer.java
@@ -0,0 +1,26 @@
+package cvut.fit.di.container;
+
+/**
+ *
+ * Hlavni API trida.
+ *
+ */
+public class DIContainer {
+
+
+    /**
+     * Inicializace probiha po zavolani konstruktoru.
+     */
+    public void DIContainer() {
+        init();
+    }
+
+
+    /**
+     * Zakladni inicializacni metoda.
+     * Vola buildera, ktery vytvori objektovy graf.
+     */
+    private void init() {
+
+    }
+}
